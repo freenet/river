cb6836f30a453f1fd0680efd4a9b89a49a178923
Ian Clarke <ian.clarke@gmail.com>
2024-08-17 16:15:23 -0500

    fix: Handle invalid signature in state tests

--- common/src/state.rs
+++ common/src/state.rs
@@ -274,7 +274,7 @@ mod tests {
                         215, 90, 152, 1, 130, 177, 10, 183, 213, 75, 254, 211, 201, 100, 7, 58, 
                         14, 225, 114, 243, 218, 166, 35, 37, 175, 2, 26, 104, 247, 7, 81, 27
                     ]).expect("Invalid public key"),
-                    signature: Signature::from_bytes(&[0; 64]).expect("Invalid signature"),
+                    signature: Signature::from_bytes(&[0; 64]),
                 });
                 set
             },
