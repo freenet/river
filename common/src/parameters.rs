68e813c23adcc8ad82b2111a5e8ec9278d9123c0
Ian Clarke <ian.clarke@gmail.com>
2024-08-17 20:20:54 -0500

    feat: Add owner_member_id method to ChatRoomParameters

--- common/src/parameters.rs
+++ common/src/parameters.rs
@@ -1,7 +1,15 @@
 use ed25519_dalek::VerifyingKey;
 use serde::{Deserialize, Serialize};
+use crate::state::MemberId;
+use crate::util::fast_hash;
 
 #[derive(Serialize, Deserialize)]
 pub struct ChatRoomParameters {
     pub owner: VerifyingKey,
+}
+
+impl ChatRoomParameters {
+    pub fn owner_member_id(&self) -> MemberId {
+        MemberId(fast_hash(&self.owner.to_bytes()))
+    }
 }
