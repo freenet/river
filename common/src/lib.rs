5458b1d7dac16e09c03a7d9c432e17c08aa1b4cf
Ian Clarke <ian.clarke@gmail.com>
2024-08-18 12:13:06 -0500

    refactor: Remove unused modules from common library

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,9 +1,4 @@
 pub mod state;
-pub mod configuration;
-pub mod member;
-pub mod upgrade;
-pub mod message;
-pub mod ban;
 pub mod summary;
 pub mod parameters;
 pub mod delta;
@@ -13,15 +8,3 @@ pub use state::ChatRoomState;
 pub use summary::ChatRoomSummary;
 pub use parameters::ChatRoomParameters;
 pub use delta::ChatRoomDelta;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
-
-#[cfg(test)]
-mod proptest_tests;
