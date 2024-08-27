34be8ce4d4b3d966ac91a2e0ec4256b49e032caa
Ian Clarke <ian.clarke@gmail.com>
2024-08-27 13:41:52 -0500

    annotate generated structs with appropriate derives

--- scaffold-macro/src/lib.rs
+++ scaffold-macro/src/lib.rs
@@ -20,38 +20,75 @@ pub fn composable(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let field_names: Vec<_> = fields.iter().map(|f| &f.ident).collect();
     let field_types: Vec<_> = fields.iter().map(|f| &f.ty).collect();
 
+    // Take the type of the first field to derive ParentState and Parameters
+    let first_field_type = &field_types[0];
+
     let summary_name = format_ident!("{}Summary", name);
     let delta_name = format_ident!("{}Delta", name);
 
     let summary_fields = field_names.iter().zip(field_types.iter()).map(|(name, ty)| {
-        quote! { #name: <#ty as ComposableState>::Summary }
+        quote! {
+            #name: <#ty as ComposableState>::Summary
+        }
     });
 
     let delta_fields = field_names.iter().zip(field_types.iter()).map(|(name, ty)| {
-        quote! { #name: <#ty as ComposableState>::Delta }
+        quote! {
+            #name: <#ty as ComposableState>::Delta
+        }
     });
 
-    // Modify Parameters to reuse the existing type from one of the fields (e.g., AuthorizedConfiguration)
-    // Assuming the parameters type is the same for all fields, we only reference one field's Parameters
-    let parameters_type = quote! { <AuthorizedConfiguration as ComposableState>::Parameters };
+    // Error messages for missing ComposableState implementation
+    let check_composable_impls = field_types.iter().map(|ty| {
+        quote! {
+            const _: fn() = || {
+                fn check_composable<T: ComposableState>() {}
+                check_composable::<#ty>();
+            };
+        }
+    });
+
+    // Ensure that all fields share the same ParentState and Parameters
+    let check_matching_parent_state = field_types.iter().map(|ty| {
+        quote! {
+            const _: fn() = || {
+                fn check_parent_state<T: ComposableState<ParentState = <#first_field_type as ComposableState>::ParentState>>() {}
+                check_parent_state::<#ty>();
+            };
+        }
+    });
+
+    let check_matching_parameters = field_types.iter().map(|ty| {
+        quote! {
+            const _: fn() = || {
+                fn check_parameters<T: ComposableState<Parameters = <#first_field_type as ComposableState>::Parameters>>() {}
+                check_parameters::<#ty>();
+            };
+        }
+    });
 
     let verify_impl = field_names.iter().map(|name| {
         quote! {
             self.#name.verify(parent_state, parameters)?;
-    }
+        }
     });
 
-
     let summarize_impl = field_names.iter().map(|name| {
-        quote! { #name: self.#name.summarize(parent_state, parameters) }
+        quote! {
+            #name: self.#name.summarize(parent_state, parameters)
+        }
     });
 
     let delta_impl = field_names.iter().map(|name| {
-        quote! { #name: self.#name.delta(parent_state, parameters, &old_state_summary.#name) }
+        quote! {
+            #name: self.#name.delta(parent_state, parameters, &old_state_summary.#name)
+        }
     });
 
     let apply_delta_impl = field_names.iter().map(|name| {
-        quote! { #name: self.#name.apply_delta(parent_state, parameters, &delta.#name) }
+        quote! {
+            #name: self.#name.apply_delta(parent_state, parameters, &delta.#name)
+        }
     });
 
     let generic_params: Vec<_> = input.generics.params.iter().collect();
@@ -63,12 +100,13 @@ pub fn composable(_attr: TokenStream, item: TokenStream) -> TokenStream {
 
         #input
 
-        #[derive(serde::Serialize, serde::Deserialize, Clone)]
+        // Automatically implement Serialize, Deserialize, Clone, PartialEq, and Debug for the generated Summary and Delta structs
+        #[derive(serde::Serialize, serde::Deserialize, Clone, PartialEq)]
         pub struct #summary_name #ty_generics #where_clause {
             #(#summary_fields,)*
         }
 
-        #[derive(serde::Serialize, serde::Deserialize, Clone)]
+        #[derive(serde::Serialize, serde::Deserialize, Clone, PartialEq)]
         pub struct #delta_name #ty_generics #where_clause {
             #(#delta_fields,)*
         }
@@ -80,7 +118,7 @@ pub fn composable(_attr: TokenStream, item: TokenStream) -> TokenStream {
             type ParentState = #name;
             type Summary = #summary_name #ty_generics;
             type Delta = #delta_name #ty_generics;
-            type Parameters = #parameters_type;
+            type Parameters = <#first_field_type as ComposableState>::Parameters;
 
             fn verify(&self, parent_state: &Self::ParentState, parameters: &Self::Parameters) -> Result<(), String> {
                 #(#verify_impl)*
@@ -105,10 +143,12 @@ pub fn composable(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 }
             }
         }
-    };
 
-    println!("**************");
-    println!("{}", expanded);
+        // Additional checks to provide better compile-time error messages
+        #(#check_composable_impls)*
+        #(#check_matching_parent_state)*
+        #(#check_matching_parameters)*
+    };
 
     TokenStream::from(expanded)
 }
