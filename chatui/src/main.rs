f274af4942a3c328ce11fb26b964ba2c488974fb
Ian Clarke <ian.clarke@gmail.com>
2024-09-12 13:17:01 -0500

    Revert "feat: add Bulma CSS to Dioxus application"
    This reverts commit 253bf12c940d4dd2491ec83905a8ce60711e4eb6.

--- chatui/src/main.rs
+++ chatui/src/main.rs
@@ -2,7 +2,6 @@
 
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{Level, info};
-use dioxus_web::Config;
 
 mod components;
 use components::app::App;
@@ -11,14 +10,5 @@ fn main() {
     // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
     info!("starting app");
-
-    // Configure the application
-    let config = Config::new().with_default_head(
-        r#"
-        <link rel="stylesheet" href="/assets/bulma.min.css">
-        "#
-    );
-
-    // Launch the app with the custom configuration
-    dioxus_web::launch_with_props(App, (), config);
+    launch(App);
 }
