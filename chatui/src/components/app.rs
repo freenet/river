c0df3a8d5ead2079d5fc2ef98efd06fcf95c68e7
Ian Clarke <ian.clarke@gmail.com>
2024-09-19 19:23:44 -0500

    feat: Add ChatState context provider to App component

--- chatui/src/components/app.rs
+++ chatui/src/components/app.rs
@@ -3,8 +3,7 @@ use crate::models::ChatState;
 use super::{chat_rooms::ChatRooms, main_chat::MainChat, user_list::MemberList, modal::Modal};
 
 pub fn App() -> Element {
-    let chat_state = use_context_provider(|| ChatState::new());
-    
+    let chat_state = use_context_provider(|| ChatState::new());    
     rsx! {
         div { class: "chat-container",
             ChatRooms {}
