3560684522fbf93ee7a418f0f8a67400120f83a2
Ian Clarke <ian.clarke@gmail.com>
2024-09-24 17:08:43 -0500

    Revert "feat: Implement chat UI structure"
    This reverts commit 8e20b21f60b39ea9e5449a588b957ea381909278.

--- chatui/src/components/app.rs
+++ chatui/src/components/app.rs
@@ -4,6 +4,7 @@ use dioxus::prelude::*;
 use ed25519_dalek::{SigningKey, VerifyingKey};
 use common::ChatRoomStateV1;
 use crate::example_data::create_example_room;
+use super::{chat_rooms::ChatRooms, main_chat::MainChat, user_list::MemberList, modal::Modal};
 
 pub fn App() -> Element {
     let rooms: Signal<HashMap<VerifyingKey, (ChatRoomStateV1, Option<SigningKey>)>> = use_signal(|| {
@@ -11,7 +12,8 @@ pub fn App() -> Element {
         let (verifying_key, room_state) = create_example_room();
         map.insert(verifying_key, (room_state, None));
         map
-    });
+    }
+    );
     let current_room: Signal<Option<VerifyingKey>> = use_signal(|| None);
     let current_room_state: Memo<Option<ChatRoomStateV1>> = use_memo(move || {
         current_room().and_then(|current_room_key| {
@@ -21,94 +23,21 @@ pub fn App() -> Element {
 
     rsx! {
         div { class: "chat-container",
-            // Chat Rooms
-            aside { class: "chat-rooms has-background-light",
-                div { class: "logo-container",
-                    img { class: "logo", src: "/api/placeholder/125/92", alt: "Freenet Logo" }
-                }
-                div { class: "menu p-4", style: "flex-grow: 1; display: flex; flex-direction: column;",
-                    p { class: "menu-label", "Chat Rooms" }
-                    ul { class: "menu-list", style: "flex-grow: 1; overflow-y: auto;",
-                        li {
-                            div { class: "is-active is-flex is-justify-content-space-between",
-                                a { "General" }
-                                span { class: "more-info", onclick: move |_| { /* TODO: Implement modal */ },
-                                    i { class: "fas fa-ellipsis-h" }
-                                }
-                            }
-                        }
-                        // Add more rooms here
-                    }
-                    div { class: "add-button",
-                        button { onclick: move |_| { /* TODO: Implement add room */ },
-                            span { class: "icon is-small", i { class: "fas fa-plus" } }
-                            span { "Add Room" }
-                        }
-                    }
-                }
+            ChatRooms {
+                rooms: rooms,
+                current_room: current_room
             }
-
-            // Main Chat
-            div { class: "main-chat",
-                div { class: "chat-messages",
-                    div { class: "box",
-                        strong { "Alice:" }
-                        " Welcome to Freenet Chat! How's everyone doing?"
-                    }
-                    // Add more messages here
-                }
-                div { class: "new-message",
-                    div { class: "field has-addons",
-                        div { class: "control is-expanded",
-                            input { class: "input", type: "text", placeholder: "Type your message..." }
-                        }
-                        div { class: "control",
-                            button { class: "button is-primary", "Send" }
-                        }
-                    }
-                }
+            MainChat {
+                current_room: current_room,
+                current_room_state: current_room_state
             }
-
-            // User List
-            aside { class: "user-list has-background-light",
-                div { class: "menu p-4", style: "height: 100%; display: flex; flex-direction: column;",
-                    p { class: "menu-label", "Users in Room" }
-                    ul { class: "menu-list", style: "flex-grow: 1; overflow-y: auto;",
-                        li {
-                            div { class: "is-flex is-justify-content-space-between",
-                                span { "Alice" }
-                                span { class: "more-info", onclick: move |_| { /* TODO: Implement modal */ },
-                                    i { class: "fas fa-ellipsis-h" }
-                                }
-                            }
-                        }
-                        // Add more users here
-                    }
-                    div { class: "add-button",
-                        button { onclick: move |_| { /* TODO: Implement invite user */ },
-                            span { class: "icon is-small", i { class: "fas fa-user-plus" } }
-                            span { "Invite User" }
-                        }
-                    }
-                }
+            MemberList {
+                current_room: current_room,
+                current_room_state: current_room_state
             }
-
-            // Modal (placeholder, implement actual modal component later)
-            div { id: "infoModal", class: "modal",
-                div { class: "modal-background" }
-                div { class: "modal-card",
-                    header { class: "modal-card-head",
-                        p { class: "modal-card-title", id: "modalTitle" }
-                        button { class: "delete", aria_label: "close" }
-                    }
-                    section { class: "modal-card-body",
-                        div { id: "modalContent" }
-                    }
-                    footer { class: "modal-card-foot",
-                        button { class: "button is-success", "Save changes" }
-                        button { class: "button", "Cancel" }
-                    }
-                }
+            Modal {
+                current_room: current_room,
+                current_room_state: current_room_state
             }
         }
     }
