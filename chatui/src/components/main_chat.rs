e2b83f9d19a4907f15d340f6573b674323dd35fe
Ian Clarke <ian.clarke@gmail.com>
2024-09-27 12:31:08 -0500

    Revert "fix: Render chat messages correctly"
    This reverts commit a0d386104a934fd54aa9148b0bc073ce75ed0af3.

--- chatui/src/components/main_chat.rs
+++ chatui/src/components/main_chat.rs
@@ -31,15 +31,17 @@ pub fn MainChat(
         div { class: "main-chat",
             div { class: "chat-messages",
                 {current_room_state.read().as_ref().map(|room_state| {
-                    room_state.recent_messages.messages.iter().map(|message| {
-                        rsx! {
-                            MessageItem {
-                                key: "{message.id().0:?}",
-                                message: message.clone(),
-                                member_info: room_state.member_info.clone()
+                    rsx! {
+                        {room_state.recent_messages.messages.iter().map(|message| {
+                            rsx! {
+                                MessageItem {
+                                    key: "{message.id().0:?}",
+                                    message: message.clone(),
+                                    member_info: room_state.member_info.clone()
+                                }
                             }
-                        }
-                    }).collect::<Vec<_>>()
+                        })}
+                    }
                 })}
             }
             div { class: "new-message",
