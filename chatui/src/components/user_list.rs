0d88eff9411aa3aaa1699f3a70904b5b3eeb903b
Ian Clarke <ian.clarke@gmail.com>
2024-09-28 15:22:04 -0500

    fix: Refactor user list component

--- chatui/src/components/user_list.rs
+++ chatui/src/components/user_list.rs
@@ -19,20 +19,23 @@ pub fn MemberList(
                 p { class: "menu-label", "Users in Room" },
                 ul { class: "menu-list", style: "flex-grow: 1; overflow-y: auto;",
                     {
-                    let members_lock = members.read();
-                    if let Some((member_info, members)) = &*members_lock {
-                        for member in &members.members {
-                            let member_info = member_info.member_info.iter().find(|mi| mi.member_info.member_id == member.member.owner_member_id);
-                            let nickname = member_info.map(|mi| mi.member_info.preferred_nickname.as_str()).unwrap_or("Unknown");
-
+                    members.read().as_ref().map(|members| {
+                        let member_info = &members.0;
+                        let members = &members.1;
+                        members.members.iter().map(|member| {
+                            let member_info = member_info.member_info.iter().find(|mi| mi.member_id == member.member.owner_member_id).unwrap();
+                            rsx! {
                                 li {
-                                    class: "user-list-item", key: "{member.member.member_id}",
-                                    span { class: "icon is-small", i { class: "fas fa-user" } },
-                                    span { "{nickname}" },
-                                },
-                        }
-                    }
-                        }
+                                    class: "user-list-item",
+                                    div { class: "user-list",
+                                        span { class: "icon is-small",
+                                            i { class: "fas fa-user" }
+                                        },
+                                        span { "{member_info.preferred_nickname}" }
+                                    }
+                                    }
+                                }
+                    })
                 }
                 div { class: "add-button mt-4",
                     button { class: "button is-small is-fullwidth",
