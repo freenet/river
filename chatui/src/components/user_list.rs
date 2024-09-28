8c4bc8a97a2eeec289562896436e1ccc997b7343
Ian Clarke <ian.clarke@gmail.com>
2024-09-27 21:53:03 -0500

    feat: Implement user list component

--- chatui/src/components/user_list.rs
+++ chatui/src/components/user_list.rs
@@ -1,42 +1,42 @@
+use common::state::member::MembersV1;
+use common::state::member_info::MemberInfoV1;
+use common::ChatRoomStateV1;
 use dioxus::prelude::*;
 use ed25519_dalek::VerifyingKey;
-use common::ChatRoomStateV1;
 
 #[component]
 pub fn MemberList(
     current_room: Signal<Option<VerifyingKey>>,
-    current_room_state: Memo<Option<ChatRoomStateV1>>
+    current_room_state: Memo<Option<ChatRoomStateV1>>,
 ) -> Element {
+    let members: Memo<(MemberInfoV1, MembersV1)> = use_memo(move || {
+        let read_state = current_room_state();
+        let room_state = read_state.as_ref().unwrap();
+        (room_state.member_info.clone(), room_state.members.clone())
+    });
+
     rsx! {
         aside { class: "user-list has-background-light",
             div { class: "menu p-4", style: "height: 100%; display: flex; flex-direction: column;",
                 p { class: "menu-label", "Users in Room" }
-                ul { class: "menu-list", style: "flex-grow: 1; overflow-y: auto;",
-                    {current_room_state.read().as_ref().map(|room_state| {
-                        let members = &room_state.members.members;
-                        let member_info = &room_state.member_info.member_info;
-                        
-                        members.iter().map(|member| {
-                            let member_id = member.member.id();
-                            let nickname = member_info
-                                .iter()
-                                .find(|info| info.member_info.member_id == member_id)
-                                .map(|info| info.member_info.preferred_nickname.clone())
-                                .unwrap_or_else(|| format!("User {:?}", member_id));
-                            
-                            rsx! {
-                                li { key: "{member_id:?}",
-                                    div { class: "is-flex is-align-items-center",
-                                        span { class: "icon is-small mr-2",
-                                            i { class: "fas fa-user" }
-                                        }
-                                        span { "{nickname}" }
+                ul { class: "menu-list", style: "flex-grow: 1; overflow-y: auto;", {
+                    members.read().0.member_info.iter().map(|auth_member_info| {
+                        let member_info = auth_member_info.member_info.clone();
+                      //  let _member = members.read().1.members.iter().find(|m| m.member.id() == member_info.member_id).unwrap();
+                        rsx! {
+                            li {
+                                key: "{member_info.member_id}",
+                                class: "user-list-item",
+                                div { class: "user-list",
+                                    span { class: "icon is-small",
+                                        i { class: "fas fa-user" }
                                     }
+                                    span { "{member_info.preferred_nickname}" }
                                 }
                             }
-                        }).collect::<Vec<_>>()
-                    }).unwrap_or_default()
-                }
+                        }
+                    }).collect::<Vec<Element>>().into_iter()
+                }}
                 div { class: "add-button mt-4",
                     button { class: "button is-small is-fullwidth",
                         onclick: move |_| {
