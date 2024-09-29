c5f68bb25374c5c4b8e7f6dd5551c0363e60958b
Ian Clarke <ian.clarke@gmail.com>
2024-09-29 17:28:15 -0500

    Revert "fix: Implement scrolling behavior for new messages in the MainChat component"
    This reverts commit fb84aabd3d1cf74c6fa963fbb73f0d69e2cb7a3f.

--- chatui/src/components/main_chat.rs
+++ chatui/src/components/main_chat.rs
@@ -1,13 +1,18 @@
 use crate::components::app::{CurrentRoom, Rooms};
+use crate::util::get_current_system_time;
 use chrono::{DateTime, Utc};
+use common::state::member::MemberId;
 use common::state::member_info::MemberInfoV1;
-use common::state::message::AuthorizedMessageV1;
+use common::state::message::{AuthorizedMessageV1, MessageV1};
+use common::state::{ChatRoomParametersV1, ChatRoomStateV1Delta};
 use dioxus::prelude::*;
+use dioxus_logger::tracing::{info, warn};
+use freenet_scaffold::ComposableState;
 use web_sys::HtmlElement;
 
 #[component]
 pub fn MainChat() -> Element {
-    let rooms = use_context::<Signal<Rooms>>();
+    let mut rooms = use_context::<Signal<Rooms>>();
     let current_room = use_context::<Signal<CurrentRoom>>();
     let current_room_data = use_memo(move || match current_room.read().owner_key {
         Some(owner_key) => rooms.read().map.get(&owner_key).map(|rd| rd.clone()),
@@ -27,11 +32,11 @@ pub fn MainChat() -> Element {
             })
             .unwrap_or_else(|| "No Room Selected".to_string())
     });
-    let new_message = use_signal(String::new);
-    let chat_messages_ref = use_ref(NodeRef::new);
+    let mut new_message = use_signal(String::new);
+    let chat_messages_ref = use_node_ref();
 
     use_effect(move || {
-        if let Some(messages_element) = chat_messages_ref.read().get() {
+        if let Some(messages_element) = chat_messages_ref.get() {
             let messages_element: &HtmlElement = messages_element.unchecked_ref();
             messages_element.set_scroll_top(messages_element.scroll_height());
         }
@@ -44,7 +49,7 @@ pub fn MainChat() -> Element {
             }
             div { 
                 class: "chat-messages flex-grow-1 overflow-auto",
-                ref: chat_messages_ref.read().clone(),
+                ref: chat_messages_ref,
                 {
                     current_room_data.read().as_ref().map(|room_data| {
                     let room_state = room_data.room_state.clone();
