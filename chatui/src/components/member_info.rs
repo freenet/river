b460b89434c66b81d60880fcd66ea7145e593a57
Ian Clarke <ian.clarke@gmail.com>
2024-10-06 15:11:08 -0500

    compiles, haven't tested yet

--- chatui/src/components/member_info.rs
+++ chatui/src/components/member_info.rs
@@ -2,101 +2,107 @@ mod nickname_field;
 
 use crate::components::app::{CurrentRoom, Rooms};
 use crate::util::get_current_room_data;
-use common::state::member::{AuthorizedMember, MemberId, MembersV1};
-use common::state::member_info::{AuthorizedMemberInfo, MemberInfoV1};
+use common::state::member::MemberId;
 use dioxus::prelude::*;
+use nickname_field::NicknameField;
 
 #[component]
 pub fn MemberInfo(member_id: MemberId, is_active: Signal<bool>) -> Element {
+    // Retrieve context signals
     let rooms = use_context::<Signal<Rooms>>();
     let current_room = use_context::<Signal<CurrentRoom>>();
     let current_room_state = get_current_room_data(rooms, current_room);
-    let members: Memo<Option<(MemberInfoV1, Option<MembersV1>)>> = use_memo(move || {
-        current_room_state
-            .read()
-            .as_ref()
-            .map(|room_state| (room_state.room_state.member_info.clone(), Some(room_state.room_state.members.clone())))
-    });
-    
-    let member: Memo<Option<(Option<AuthorizedMember>, AuthorizedMemberInfo)>> = use_memo(move || {
-        if let Some((member_info, members)) = members.read().as_ref() {
-            if let Some(member_info) = member_info.member_info.iter().find(|mi| mi.member_info.member_id == member_id).cloned() {
-                if let Some(members) = members {
-                    if let Some(member) = members.members.iter().find(|member| member.member.owner_member_id == member_id).cloned() {
-                        return Some((Some(member), member_info));
-                    }
-                }
-                // If member is not found in MembersV1, it might be the room owner
-                return Some((None, member_info));
-            }
+
+    // Read the current room state
+    let current_room_state_read = current_room_state.read();
+    let room_state = match current_room_state_read.as_ref() {
+        Some(state) => state,
+        None => {
+            return rsx! { div { "Room state not available" } };
         }
-        None
-    });
+    };
 
-    let member_read = member.read();
-    if member_read.as_ref().is_none() {
-        return rsx! {
-            div { "Member not found (this shouldn't happen)" }
-        };
-    }
+    // Extract member info and members list
+    let member_info_list = &room_state.room_state.member_info.member_info;
+    let members_list = &room_state.room_state.members.members;
+    let owner_key = current_room.read().owner_key;
 
-    let (member, member_info) = member_read.as_ref().unwrap();
+    // Find the AuthorizedMemberInfo for the given member_id
+    let member_info = match member_info_list.iter().find(|mi| mi.member_info.member_id == member_id) {
+        Some(mi) => mi,
+        None => {
+            return rsx! { div { "Member not found (this shouldn't happen)" } };
+        }
+    };
 
+    // Try to find the AuthorizedMember for the given member_id
+    let member = members_list.iter().find(|m| m.member.owner_member_id == member_id);
+
+    // Determine if the member is the room owner
     let is_owner = member.is_none();
-    let current_room_read = current_room.read();
-    let owner_key = current_room_read.owner_key;
 
+    // Get the inviter's nickname
     let invited_by = if is_owner {
         "N/A (Room Owner)".to_string()
     } else {
-        let invited_by = member.as_ref().unwrap().member.invited_by;
-        let members_read = members.read();
-        members_read.as_ref().and_then(|(member_info, _)| {
-            member_info.member_info.iter().find(|mi| mi.member_info.member_id == invited_by).map(|mi| mi.member_info.preferred_nickname.clone())
-        }).unwrap_or_else(|| "Unknown".to_string())
+        let inviter_id = member.unwrap().member.invited_by;
+        member_info_list
+            .iter()
+            .find(|mi| mi.member_info.member_id == inviter_id)
+            .map(|mi| mi.member_info.preferred_nickname.clone())
+            .unwrap_or_else(|| "Unknown".to_string())
+    };
+
+    // Get the member ID string to display
+    let member_id_str = if is_owner {
+        owner_key
+            .map(|vk| MemberId::new(&vk).to_string())
+            .unwrap_or_else(|| "Unknown".to_string())
+    } else {
+        member.unwrap().member.owner_member_id.to_string()
     };
 
     rsx! {
         div {
             class: if *is_active.read() { "modal is-active" } else { "modal" },
-            div { class: "modal-background",
-                    onclick: move |_| {
+            div {
+                class: "modal-background",
+                onclick: move |_| {
                     is_active.set(false);
                 }
             },
-            div { class: "modal-content",
-                div { class: "box",
+            div {
+                class: "modal-content",
+                div {
+                    class: "box",
                     h1 { "Member Info" }
-                    
-                    if let Some(member) = member {
-                        rsx! {
-                            nickname_field::NicknameField {
-                                member: member.clone(),
-                                member_info: member_info.clone()
-                            }
+
+                    if !is_owner {
+                        NicknameField {
+                            member: member.unwrap().clone(),
+                            member_info: member_info.clone()
                         }
                     } else {
-                        rsx! {
-                            div { "Room Owner" }
-                        }
+                        div { "Room Owner" }
                     }
-                    div { class: "field",
+
+                    div {
+                        class: "field",
                         label { class: "label", "Member ID" }
-                        div { class: "control",
+                        div {
+                            class: "control",
                             input {
                                 class: "input",
-                                value: if is_owner {
-                                    owner_key.map(|vk| MemberId::new(&vk).to_string()).unwrap_or_else(|| "Unknown".to_string())
-                                } else {
-                                    member.as_ref().unwrap().member.owner_member_id.to_string()
-                                },
+                                value: member_id_str,
                                 readonly: true
                             }
                         }
                     }
-                    div { class: "field",
+                    div {
+                        class: "field",
                         label { class: "label", "Invited by" }
-                        div { class: "control",
+                        div {
+                            class: "control",
                             input {
                                 class: "input",
                                 value: invited_by,
@@ -105,9 +111,11 @@ pub fn MemberInfo(member_id: MemberId, is_active: Signal<bool>) -> Element {
                         }
                     }
                     if is_owner {
-                        div { class: "field",
+                        div {
+                            class: "field",
                             label { class: "label", "Role" }
-                            div { class: "control",
+                            div {
+                                class: "control",
                                 input {
                                     class: "input",
                                     value: "Room Owner",
@@ -118,7 +126,8 @@ pub fn MemberInfo(member_id: MemberId, is_active: Signal<bool>) -> Element {
                     }
                 }
             },
-            button { class: "modal-close is-large",
+            button {
+                class: "modal-close is-large",
                 onclick: move |_| {
                     is_active.set(false);
                 }
