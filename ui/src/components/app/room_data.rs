dd905383790127aa7db07d2a4bca7a63384abbe8
Ian Clarke <ian.clarke@gmail.com>
2024-10-10 13:00:13 -0500

    feat: Add room_data.rs component

---
+++ ui/src/components/app/room_data.rs
@@ -0,0 +1,58 @@
+use common::room_state::member::MemberId;
+use super::*;
+
+#[derive(Clone)]
+pub struct RoomData {
+    pub room_state: ChatRoomStateV1,
+    pub user_signing_key: Option<SigningKey>,
+}
+
+impl RoomData {
+    pub fn can_send_message(&self) -> Result<(), String> {
+        // Must have a user signing key to send a message
+        match &self.user_signing_key {
+            Some(signing_key) => {
+                let verifying_key = signing_key.verifying_key();
+                // Must be a member of the room to send a message
+                if self.room_state.members.members.iter().any(|m| m.member.member_vk == verifying_key) {
+                    // Must not be banned from the room to send a message
+                    if self.room_state.bans.0.iter().any(|b| b.ban.banned_user == MemberId::new(&verifying_key)) {
+                        Err("User is banned from the room".to_string())
+                    } else {
+                        Ok(())
+                    }
+                } else {
+                    Err("User is not a member of the room".to_string())
+                }
+            }
+            None => Err("User signing key is not set".to_string()),
+        }
+    }
+}
+
+impl PartialEq for RoomData {
+    fn eq(&self, other: &Self) -> bool {
+        self.room_state == other.room_state
+    }
+}
+
+pub struct CurrentRoom {
+    pub owner_key: Option<VerifyingKey>,
+}
+
+impl PartialEq for CurrentRoom {
+    fn eq(&self, other: &Self) -> bool {
+        self.owner_key == other.owner_key
+    }
+}
+
+#[derive(Clone)]
+pub struct Rooms {
+    pub map: HashMap<VerifyingKey, RoomData>,
+}
+
+impl PartialEq for Rooms {
+    fn eq(&self, other: &Self) -> bool {
+        self.map == other.map
+    }
+}
