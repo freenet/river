d2f5d33af3c71b2bec54388983093cd4e8bd8edf
Ian Clarke <ian.clarke@gmail.com>
2024-10-11 16:39:55 -0500

    feat: Add example room for invite

--- ui/src/components/app.rs
+++ ui/src/components/app.rs
@@ -1,5 +1,5 @@
 use super::{chat_rooms::ChatRooms, main_chat::MainChat, member_list::MemberList};
-use crate::example_data::create_example_room;
+use crate::example_data::{create_example_room, create_example_room_for_invite};
 use crate::global_context::UserInfoModals;
 use dioxus::prelude::*;
 use std::collections::HashMap;
@@ -8,8 +8,15 @@ use crate::room_data::{CurrentRoom, Rooms};
 pub fn App() -> Element {
     use_context_provider(|| {
         let mut map = HashMap::new();
-        let (owner_key, room_data) = create_example_room();
-        map.insert(owner_key, room_data);
+        {
+            let (owner_key, room_data) = create_example_room();
+            map.insert(owner_key, room_data);
+        }
+        {
+            let (owner_key, room_data) = create_example_room_for_invite();
+            map.insert(owner_key, room_data);
+        }
+            
         Signal::new(Rooms { map })
     });
     use_context_provider(|| Signal::new(CurrentRoom { owner_key: None }));
