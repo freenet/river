518c70b7adb92ccd36466299e0d666a1b4a9b4b6
Ian Clarke <ian.clarke@gmail.com>
2024-10-15 10:43:40 -0500

    revert aider mess

--- ui/src/components/chat_rooms.rs
+++ ui/src/components/chat_rooms.rs
@@ -1,32 +1,12 @@
-mod edit_room_modal;
-
 use dioxus::prelude::*;
 use dioxus_free_icons::icons::fa_solid_icons::FaComments;
 use dioxus_free_icons::Icon;
 use crate::room_data::{CurrentRoom, Rooms};
-use self::edit_room_modal::EditRoomModal;
-use ed25519_dalek::VerifyingKey;
 
 #[component]
 pub fn ChatRooms() -> Element {
     let rooms = use_context::<Signal<Rooms>>();
     let current_room = use_context::<Signal<CurrentRoom>>();
-    let edit_modal_active = use_signal(|| None::<VerifyingKey>);
-
-    let on_save = move |(room_key, name, description)| {
-        rooms.with_mut(|rooms| {
-            if let Some(room) = rooms.map.get_mut(&room_key) {
-                room.room_state.configuration.configuration.name = name;
-                room.room_state.configuration.configuration.description = description;
-            }
-        });
-        edit_modal_active.set(None);
-    };
-
-    let on_cancel = move |_| {
-        edit_modal_active.set(None);
-    };
-
     rsx! {
         aside { class: "chat-rooms",
             div { class: "logo-container",
@@ -49,36 +29,22 @@ pub fn ChatRooms() -> Element {
                     let room_key = *room_key;
                     let room_name = room_data.room_state.configuration.configuration.name.clone();
                     let is_current = current_room.read().owner_key == Some(room_key);
+                    let mut current_room_clone = current_room.clone(); // Clone the Signal
                     rsx! {
                         li {
                             key: "{room_key:?}",
                             class: if is_current { "chat-room-item active" } else { "chat-room-item" },
-                            div {
-                                class: "room-item-container",
-                                button {
-                                    class: "room-name-button",
-                                    onclick: move |_| {
-                                        current_room.set(CurrentRoom { owner_key: Some(room_key) });
-                                    },
-                                    "{room_name}"
-                                }
-                                button {
-                                    class: "button is-small edit-room-button",
-                                    onclick: move |_| {
-                                        edit_modal_active.set(Some(room_key));
-                                    },
-                                    "Edit"
-                                }
+                            button {
+                                class: "room-name-button",
+                                onclick: move |_| {
+                                    current_room_clone.set(CurrentRoom { owner_key : Some(room_key)});
+                                },
+                                "{room_name}"
                             }
                         }
                     }
-                })}
+                }).collect::<Vec<_>>().into_iter()}
             }
-        }
-        EditRoomModal {
-            active_room: edit_modal_active,
-            on_save: on_save,
-            on_cancel: on_cancel,
         }
     }
 }
