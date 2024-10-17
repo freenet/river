87e5599a6190f3ae60117db4802f1b36f604a17f
Ian Clarke <ian.clarke@gmail.com>
2024-10-17 18:36:28 -0500

    feat: memoize room configuration and user ownership

--- ui/src/components/chat_rooms/edit_room_modal.rs
+++ ui/src/components/chat_rooms/edit_room_modal.rs
@@ -1,14 +1,16 @@
+use std::ops::Deref;
 use dioxus::prelude::*;
-use ed25519_dalek::SigningKey;
 use crate::components::app::EditRoomModalActive;
-use crate::room_data::{RoomData, Rooms};
+use crate::room_data::Rooms;
 
 #[component]
 fn EditRoomModal() -> Element {
     let rooms = use_context::<Signal<Rooms>>();
-    let edit_room_signal: Signal<EditRoomModalActive> = use_context::<Signal<EditRoomModalActive>>();
-    let editing_room: Memo<Option<RoomData>> = use_memo(move || {
-        if let Some(editing_room_vk) = edit_room_signal.read().room {
+    let mut edit_room_signal = use_context::<Signal<EditRoomModalActive>>();
+
+    // Memoize the room being edited
+    let editing_room = use_memo(move || {
+        edit_room_signal.read().room.and_then(|editing_room_vk| {
             rooms.read().map.iter().find_map(|(room_vk, room_data)| {
                 if &editing_room_vk == room_vk {
                     Some(room_data.clone())
@@ -16,98 +18,69 @@ fn EditRoomModal() -> Element {
                     None
                 }
             })
-        } else {
-            None
-        }
+        })
     });
-    
-    rsx! {
-        div {
-            class: if edit_room_signal.read().active { "modal is-active" } else { "modal" },
+
+    // Memoize the room configuration
+    let room_config = use_memo(move || {
+        editing_room
+            .read()
+            .as_ref()
+            .map(|room_data| room_data.room_state.configuration.configuration.clone())
+    });
+
+    // Memoize if the current user is the owner of the room
+    let user_is_owner = use_memo(move || {
+        editing_room
+            .read()
+            .as_ref()
+            .map_or(false, |room_data| {
+                let user_vk = room_data.user_signing_key.verifying_key();
+                let room_vk = edit_room_signal.read().room.unwrap();
+                user_vk == room_vk
+            })
+    });
+
+    // Render the modal if room configuration is available
+    if let Some(config) = room_config.clone().read().deref() {
+        rsx! {
             div {
-                class: "modal-background",
-                onclick: move |_| {
-                    edit_room_signal.write().active = false;
-                }
-            }
-            div {
-                class: "modal-content",
+                class: if edit_room_signal.read().room.is_some() { "modal is-active" } else { "modal" },
                 div {
-                    class: "box",
-                    if let Some(room_data) = editing_room() {
-                        rsx! {
-                            h1 { class: "title is-4 mb-3", "Edit Room" }
-                            
-                            div {
-                                class: "field",
-                                label { class: "label is-medium", "Room Name" }
-                                div {
-                                    class: "control",
-                                    input {
-                                        class: "input",
-                                        value: "{room_data.room_state.configuration.configuration.room_name}",
-                                        readonly: true
-                                    }
-                                }
-                            }
+                    class: "modal-background",
+                    onclick: move |_| {
+                        edit_room_signal.write().room = None;
+                    }
+                }
+                div {
+                    class: "modal-content",
+                    div {
+                        class: "box",
+                        h1 { class: "title is-4 mb-3", "Member Info" }
 
+                        div {
+                            class: "field",
+                            label { class: "label is-medium", "Member ID" }
                             div {
-                                class: "field",
-                                label { class: "label is-medium", "Room ID" }
-                                div {
-                                    class: "control",
-                                    input {
-                                        class: "input",
-                                        value: "{room_data.room_state.configuration.configuration.room_id}",
-                                        readonly: true
-                                    }
+                                class: "control",
+                                input {
+                                    class: "input",
+                                    value: "{config.name}",
+                                    readonly: true
                                 }
-                            }
-
-                            div {
-                                class: "field",
-                                label { class: "label is-medium", "Max Members" }
-                                div {
-                                    class: "control",
-                                    input {
-                                        class: "input",
-                                        value: "{room_data.room_state.configuration.configuration.max_members}",
-                                        readonly: true
-                                    }
-                                }
-                            }
-
-                            div {
-                                class: "field",
-                                label { class: "label is-medium", "Owner Member ID" }
-                                div {
-                                    class: "control",
-                                    input {
-                                        class: "input",
-                                        value: "{room_data.room_state.configuration.configuration.owner_member_id}",
-                                        readonly: true
-                                    }
-                                }
-                            }
-
-                            // Add more fields as needed
-                        }
-                    } else {
-                        rsx! {
-                            div { 
-                                class: "notification is-warning",
-                                "Room information not available" 
                             }
                         }
                     }
                 }
-            }
-            button {
-                class: "modal-close is-large",
-                onclick: move |_| {
-                    edit_room_signal.write().active = false;
+                button {
+                    class: "modal-close is-large",
+                    onclick: move |_| {
+                        edit_room_signal.write().room = None;
+                    }
                 }
             }
         }
+    } else {
+        rsx! { }
     }
 }
