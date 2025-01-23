274dd7be30174122c114d1e610cd8af86de6b670
Ian Clarke <ian.clarke@gmail.com>
2025-01-23 14:50:18 -0600

    remove cruft

--- ui/src/constants.rs
+++ ui/src/constants.rs
@@ -1,10 +1,6 @@
 pub use crate::crypto_values::CryptoValue;
 pub const KEY_VERSION_PREFIX: &str = "river:v1";
 
-pub fn key_version_prefix(crypto_value: &CryptoValue) -> String {
-    crypto_value.to_encoded_string()
-}
-
 pub const ROOM_CONTRACT_WASM: &[u8] =
     include_bytes!("../../target/wasm32-unknown-unknown/release/room_contract.wasm");
 
