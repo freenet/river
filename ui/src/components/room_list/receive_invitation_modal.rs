d36be5a17b1ab0b838c6cfe14b8911f2a2d0ca69
Ian Clarke <ian.clarke@gmail.com>
2025-02-23 18:22:44 -0600

    refactor: Improve invitation modal UI and handling logic

--- ui/src/components/room_list/receive_invitation_modal.rs
+++ ui/src/components/room_list/receive_invitation_modal.rs
@@ -1,47 +1,41 @@
+use crate::components::members::Invitation;
 use crate::room_data::Rooms;
 use dioxus::prelude::*;
-use crate::components::members::Invitation;
 
 #[component]
 pub fn ReceiveInvitationModal(invitation: Signal<Option<Invitation>>) -> Element {
     let rooms = use_context::<Signal<Rooms>>();
 
     rsx! {
-        div {
-            class: if invitation.read().is_some() { "modal is-active" } else { "modal" },
-            div { class: "modal-background" }
-            div { class: "modal-content",
-                div { class: "box",
-                    if let Some(inv) = invitation.read().as_ref() {
-                        rsx! {
-                            h3 { class: "title is-4", "Received Invitation" }
-                            p { "Would you like to join this chat room?" }
-                            div { class: "field is-grouped",
-                                div { class: "control",
-                                    button {
-                                        class: "button is-primary",
-                                        onclick: move |_| {
-                                            // TODO: Handle accepting invitation
-                                            invitation.set(None);
-                                        },
-                                        "Accept"
-                                    }
-                                }
-                                div { class: "control",
-                                    button {
-                                        class: "button",
-                                        onclick: move |_| invitation.set(None),
-                                        "Decline"
-                                    }
-                                }
+            div {
+                class: if invitation.read().is_some() { "modal is-active" } else { "modal" },
+                div { class: "modal-background",
+                    onclick: move |_| invitation.set(None)
+                }
+                div { class: "modal-content",
+                    div { class: "box",
+                    
+                        h1 { class: "title", "Invitation Received" }
+                        if rooms.read().map.contains_key(&invitation.read().as_ref().unwrap().room.into()) {
+                            p { "You are already a member of this room." }
+                        } else {
+                            p { "You have been invited to join a new room." }
+                            p { "Would you like to accept the invitation?" }
+                            button {
+                                class: "button is-primary",
+                                onclick: move |_| {
+                                    // Handle accepting the invitation
+                                    // This is where you would add the logic to accept the invitation
+                                    invitation.set(None);
+                                },
+                                "Accept"
                             }
                         }
-                    }
                 }
-            }
-            button {
-                class: "modal-close is-large",
-                onclick: move |_| invitation.set(None)
+                button {
+                    class: "modal-close is-large",
+                    onclick: move |_| invitation.set(None)
+                }
             }
         }
     }
