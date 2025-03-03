d5f63523be7f900951ef0b3d37317f77ca086ec5
Ian Clarke <ian.clarke@gmail.com>
2025-03-02 20:42:12 -0600

    refactor: Remove unused response receiver in freenet synchronizer

--- ui/src/components/app/freenet_api/freenet_synchronizer.rs
+++ ui/src/components/app/freenet_api/freenet_synchronizer.rs
@@ -146,6 +146,7 @@ pub async fn initialize_connection(mut signal: Signal<FreenetSynchronizer>) -> R
             error_msg
         })?;
 
+    // Why is _response_rx not used? AI!
         let (response_tx, _response_rx) = futures::channel::mpsc::unbounded();
         let (ready_tx, ready_rx) = futures::channel::oneshot::channel();
 
