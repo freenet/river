7fd541172bc71caa12f37f1b007779f947a45381
Ian Clarke <ian.clarke@gmail.com>
2025-03-16 11:31:00 -0500

    initial work on chat-delegate

--- delegates/chat-delegate/src/lib.rs
+++ delegates/chat-delegate/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+use ed25519_dalek::{SigningKey, VerifyingKey};
+use freenet_stdlib::prelude::{delegate, DelegateError, DelegateInterface, InboundDelegateMsg, OutboundDelegateMsg, Parameters};
+use serde::{Deserialize, Serialize};
+
+pub struct RoomDelegate;
+
+#[delegate]
+impl DelegateInterface for RoomDelegate {
+
+    fn process(
+        parameters: Parameters<'static>,
+        attested: Option<&'static [u8]>,
+        message: InboundDelegateMsg
+    ) -> Result<Vec<OutboundDelegateMsg>, DelegateError> {
+        match message {
+            InboundDelegateMsg::ApplicationMessage(app_msg) => {
+                let context : Context = ciborium::from_reader(app_msg.context.as_ref())
+                    .map_err(|e| DelegateError::Deser(format!("Failed to deserialize context: {e}")))?;
+
+                todo!()
+            }
+            InboundDelegateMsg::GetSecretResponse(_get_secret_response) => {
+                todo!()
+            }
+            InboundDelegateMsg::UserResponse(_user_resposne) => {
+                todo!()
+            }
+            InboundDelegateMsg::GetSecretRequest(_get_secret_request) => {
+                todo!()
+            }
+        }
+    }
+
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+struct Context {
+    rooms : HashMap<VerifyingKey, StoredRoom>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+struct StoredRoom {
+    self_sk : SigningKey,
 }
