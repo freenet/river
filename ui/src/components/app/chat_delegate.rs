c3a50890f9614c5c325319aa31a996614ac127ae
Ian Clarke <ian.clarke@gmail.com>
2025-03-19 22:06:35 -0500

    refactor: Update chat delegate registration with new DelegateContainer and API interaction

--- ui/src/components/app/chat_delegate.rs
+++ ui/src/components/app/chat_delegate.rs
@@ -1,6 +1,6 @@
 use freenet_stdlib::client_api::DelegateRequest;
 use freenet_stdlib::client_api::ClientRequest::DelegateOp;
-use freenet_stdlib::prelude::{DelegateContainer, DelegateWasmAPIVersion};
+use freenet_stdlib::prelude::{Delegate, DelegateContainer, DelegateWasmAPIVersion};
 use dioxus::prelude::Readable;
 use crate::components::app::WEB_API;
 
@@ -9,14 +9,13 @@ pub async fn set_up_chat_delegate() {
     let delegate_bytes = include_bytes!("../../../../target/wasm32-unknown-unknown/release/chat_delegate.wasm");
     
     // Create a delegate container with the WASM bytes
-    let delegate = DelegateContainer::new_wasm(
-        delegate_bytes.to_vec(),
-        freenet_stdlib::prelude::DelegateWasmAPIVersion::V1,
+    let delegate = DelegateContainer::Wasm(
+        DelegateWasmAPIVersion::V1(Delegate::from(delegate_bytes.to_vec()))
     );
     
     // Register the delegate with the server
     // Note: For this simple delegate, we don't need encryption, so cipher and nonce are empty
-    if let Some(api) = &*WEB_API.read() {
+    if let Some(mut api) = &*WEB_API.write() {
         let _ = api.send(DelegateOp(DelegateRequest::RegisterDelegate {
             delegate,
             cipher: [0u8; 32],
