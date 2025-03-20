b96b40f91e9b55689f6e5ebcb070e84651ba7e43
Ian Clarke <ian.clarke@gmail.com>
2025-03-20 14:19:18 -0500

    refactor: Update chat delegate setup with new Delegate and DelegateContainer types

--- ui/src/components/app/chat_delegate.rs
+++ ui/src/components/app/chat_delegate.rs
@@ -1,19 +1,17 @@
 use crate::components::app::WEB_API;
 use freenet_stdlib::client_api::ClientRequest::DelegateOp;
 use freenet_stdlib::client_api::DelegateRequest;
-use freenet_stdlib::prelude::{DelegateContainer, Parameters};
-use super::chat_delegate_helpers;
+use freenet_stdlib::prelude::{Delegate, DelegateCode, DelegateContainer, DelegateWasmAPIVersion, Parameters};
 
 pub async fn set_up_chat_delegate() -> Result<(), String> {
     // Load the chat delegate WASM bytes
-    let delegate_bytes = include_bytes!("../../../../target/wasm32-unknown-unknown/release/chat_delegate.wasm");
+    let delegate_wasm = include_bytes!("../../../../target/wasm32-unknown-unknown/release/chat_delegate.wasm");
     
-    // Create empty parameters
-    let parameters = Parameters::from(vec![]);
+    let delegate_wasm = DelegateCode::from(delegate_wasm);
 
-    // Create the delegate container using our helper function
-    let delegate = chat_delegate_helpers::create_delegate_container(&delegate_bytes[..], parameters)
-        .map_err(|e| e.to_string())?;
+    let delegate = Delegate::from((delegate_wasm, Parameters::from([])));
+
+    let delegate = DelegateContainer::Wasm(DelegateWasmAPIVersion::V1(delegate));
 
     // Register the delegate with the server
     // Note: For this simple delegate, we don't need encryption, so cipher and nonce are empty
@@ -27,14 +25,4 @@ pub async fn set_up_chat_delegate() -> Result<(), String> {
     }
     
     Ok(())
-}
-
-pub fn create_chat_delegate(delegate_bytes: &[u8]) -> Result<DelegateContainer, std::io::Error> {
-    // Create empty parameters for the chat delegate
-    let parameters = Parameters::from(Vec::<u8>::new());
-
-    // Create the delegate container using our helper function
-    let delegate = chat_delegate_helpers::create_delegate_container(delegate_bytes, parameters)?;
-
-    Ok(delegate)
 }
